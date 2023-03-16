fb21f506eb16980215691c763d1bcbf0a7446a3f
Eliora Kruman <krumaneliora@gmail.com>
2023-03-15 21:06:24 -0400

    updated webapp files

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -22,10 +22,10 @@ import openai
 
 
 class GPT:
-    """make queries to gpt from a given API"""
+    '''make queries to gpt from a given API'''
 
     def __init__(self, apikey):
-        """store the apikey in an instance variable"""
+        '''store the apikey in an instance variable'''
         self.apikey = apikey
         # Set up the OpenAI API client
         openai.api_key = apikey  # os.environ.get('APIKEY')
@@ -34,7 +34,7 @@ class GPT:
         self.model_engine = "text-davinci-003"
 
     def get_response(self, prompt):
-        """Generate a GPT response"""
+        '''Generate a GPT response'''
         completion = openai.Completion.create(
             engine=self.model_engine,
             prompt=prompt,
--- ca01/gptwebapp.py
+++ ca01/gptwebapp.py
@@ -1,4 +1,4 @@
-"""
+'''
 gptwebapp shows how to create a web app which ask the user for a prompt
 and then sends it to openai's GPT API to get a response. You can use this
 as your own GPT interface and not have to go through openai's web pages.
@@ -17,39 +17,36 @@ On Windows:
 % pip install flask
 % $env:APIKEY="....." # in powershell
 % python gptwebapp.py
-"""
-from flask import request, redirect, url_for, Flask
+'''
+from flask import request,redirect,url_for,Flask
 from gpt import GPT
 import os
-import openai
-import gpt
 
 app = Flask(__name__)
-gptAPI = GPT(os.environ.get("APIKEY"))
+gptAPI = GPT(os.environ.get('APIKEY'))
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q789789uioujkkljkl...8z\n\xec]/'
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    """display a link to the general query page"""
-    print("processing / route")
-    return f"""
+    ''' display a link to the general query page '''
+    print('processing / route')
+    return f'''
         <h1>GPT Demo</h1>
         <a href="{url_for('gptdemo')}">Ask questions to GPT</a>
-    """
+    '''
 
 
-@app.route("/gptdemo", methods=["GET", "POST"])
+@app.route('/gptdemo', methods=['GET', 'POST'])
 def gptdemo():
-    """handle a get request by sending a form
-    and a post request by returning the GPT response
-    """
-    if request.method == "POST":
-        prompt = request.form["prompt"]
-        answer = gptAPI.getResponse(prompt)
-        return f"""
+    ''' handle a get request by sending a form 
+        and a post request by returning the GPT response
+    '''
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        answer = gptAPI.get_response(prompt)
+        return f'''
         <h1>GPT Demo</h1>
         <pre style="bgcolor:yellow">{prompt}</pre>
         <hr>
@@ -58,18 +55,17 @@ def gptdemo():
         Here is the answer in "pre" mode:
         <pre style="border:thin solid black">{answer}</pre>
         <a href={url_for('gptdemo')}> make another query</a>
-        """
+        '''
     else:
-        return """
+        return '''
         <h1>GPT Demo App</h1>
         Enter your query below
         <form method="post">
             <textarea name="prompt"></textarea>
             <p><input type=submit value="get response">
         </form>
-        """
+        '''
 
-
-if __name__ == "__main__":
+if __name__=='__main__':
     # run the code on port 5001, MacOS uses port 5000 for its own service :(
-    app.run(debug=True, port=5001)
+    app.run(debug=True,port=5001)
