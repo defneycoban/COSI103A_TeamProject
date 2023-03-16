9b964289a06407565dbd36dab58b65c9c4e455ed
Eliora Kruman <krumaneliora@gmail.com>
2023-03-16 13:57:53 -0400

    villain prompt

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -106,6 +106,20 @@ class GPT:
 
         response = completion.choices[0].text
         return response
+    
+    def villain(self, prompt):
+        '''The user inputs a topic, and gpt returns a character class.'''
+        completion = openai.Completion.create(
+            engine = self.model_engine,
+            prompt = 'What would be a good Dungeons and Dragons monster to fight against for:' + prompt,
+            max_tokens = 1024,
+            n = 1,
+            stop = None,
+            temperature = 0.8,
+        )
+
+        response = completion.choices[0].text
+        return response
 
 
 if __name__ == '__main__':
