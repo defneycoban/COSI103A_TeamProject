02ae63b66a8ec452c8f3809147119e1bbfe563e7
Eliora Kruman <krumaneliora@gmail.com>
2023-03-22 23:41:16 -0400

    wrote draft of process method

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -1,1 +1,44 @@
 # All printing should be done in this class. Sql commands go in transactions.py
+
+
+def process(arglist):
+    ''' examine args and make appropriate calls to the database'''
+    transaction = Transactions()
+    if arglist==[]:
+        #not implemented yet: print_usage() in tracker.py
+        print("placeholder")
+    elif arglist[0]=="quit":
+        quit()
+    elif arglist[0]=="show":
+        #not implemented yet: print_transactions(transaction.show()) in tracker.py, transaction.show() in transactions.py
+        print("placeholder")
+    elif arglist[0]=='add':
+        if len(arglist)!=5: #because there are 5 fields
+            #not implemented yet: print_usage() in tracker.py
+            print("placeholder")
+        else:
+            dictName = {'item #':arglist[1],'amount':arglist[2],'category':arglist[3],'date':arglist[4],'description':arglist[5]} in tracker.py #dictName in transactions.py
+            #not implemented yet: transaction.add(dictName) in transactions.py, dictName in transactions.py
+            print("placeholder")
+    elif arglist[0]=='delete':
+        if len(arglist)!= 2:
+            #not implemented yet: print_usage() in tracker.py
+            print("placeholder")
+        else:
+            #not implemented yet: transaction.delete(arglist[1]) in transactions.py
+            print("placeholder")
+    elif arglist[0]=="summarizeDates":
+        #not implemented yet: print_transactions(dictName = transaction.sort(date)) in tracker.py, transaction.select() in transactions.py, dictName in transactions.py
+        print("placeholder")
+    elif arglist[0]=="summarizeMonths":
+        #not implemented yet: print_transactions(dictName = transaction.sort(month)) in tracker.py, transaction.select() in transactions.py, dictName in transactions.py
+        print("placeholder")
+    elif arglist[0]=="summarizeYears":
+        #not implemented yet: print_transactions(dictName = transaction.sort(year)) in tracker.py, transaction.select() in transactions.py, dictName in transactions.py
+        print("placeholder")
+    elif arglist[0]=="print":
+        #not implemented yet: print_usage() in tracker.py
+        print("placeholder")
+    else:
+        print(arglist," is not implemented")
+        #not implemented yet: print_usage() in tracker.py
--- pa03/transactions.py
+++ pa03/transactions.py
@@ -5,3 +5,34 @@ class Transactions():
     def __init__(self):
         self.runQuery('''CREATE TABLE IF NOT EXISTS transactions 
                       (item int, amount int, date text, description text))''', ())
+
+    def process_args(arglist):
+        ''' examine args and make appropriate calls to the database'''
+        transaction = Transactions()
+        if arglist==[]:
+          print_usage()
+        elif arglist[0]=="show":
+            print_todos(todolist.selectActive())
+        elif arglist[0]=="showall":
+            print_todos(todos = todolist.selectAll())
+        elif arglist[0]=="showcomplete":
+            print_todos(todolist.selectCompleted())
+        elif arglist[0]=='add':
+            if len(arglist)!=3:
+                print_usage()
+            else:
+                todo = {'title':arglist[1],'desc':arglist[2],'completed':0}
+                todolist.add(todo)
+        elif arglist[0]=='complete':
+            if len(arglist)!= 2:
+                print_usage()
+            else:
+                todolist.setComplete(arglist[1])
+        elif arglist[0]=='delete':
+            if len(arglist)!= 2:
+                print_usage()
+            else:
+                todolist.delete(arglist[1])
+        else:
+            print(arglist,"is not implemented")
+            print_usage()
