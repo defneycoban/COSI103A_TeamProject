94e46300fa5cef48c7932506d316d4eb9f3c9887
Zev Ross <zevross@gmail.com>
2023-03-22 23:11:33 -0400

    created transactions class

--- pa03/transactions.py
+++ pa03/transactions.py
@@ -1,1 +1,7 @@
 # All sql commands should be done in this class. Print statements go in tracker.py
+
+class Transactions():
+    ''' methods for obtaining transaction data '''
+    def __init__(self):
+        self.runQuery('''CREATE TABLE IF NOT EXISTS transactions 
+                      (item int, amount int, date text, description text))''', ())
