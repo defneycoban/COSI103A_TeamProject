7ed5a24ad890093a737c3c93ba85205de2f32be8
Eliora Kruman <krumaneliora@gmail.com>
2023-03-22 23:55:43 -0400

    fixed transactions.py

--- pa03/transactions.py
+++ pa03/transactions.py
@@ -1,39 +1,13 @@
 # All sql commands should be done in this class. Print statements go in tracker.py
 
-# class and init method created by Zev
 class Transactions():
     ''' methods for obtaining transaction data '''
+
     def __init__(self):
+        ''' initialize the database '''
         self.runQuery('''CREATE TABLE IF NOT EXISTS transactions 
                       (item int, amount int, date text, description text))''', ())
-
-    def process_args(arglist):
-        ''' examine args and make appropriate calls to the database'''
-        transaction = Transactions()
-        if arglist==[]:
-          print_usage()
-        elif arglist[0]=="show":
-            print_todos(todolist.selectActive())
-        elif arglist[0]=="showall":
-            print_todos(todos = todolist.selectAll())
-        elif arglist[0]=="showcomplete":
-            print_todos(todolist.selectCompleted())
-        elif arglist[0]=='add':
-            if len(arglist)!=3:
-                print_usage()
-            else:
-                todo = {'title':arglist[1],'desc':arglist[2],'completed':0}
-                todolist.add(todo)
-        elif arglist[0]=='complete':
-            if len(arglist)!= 2:
-                print_usage()
-            else:
-                todolist.setComplete(arglist[1])
-        elif arglist[0]=='delete':
-            if len(arglist)!= 2:
-                print_usage()
-            else:
-                todolist.delete(arglist[1])
-        else:
-            print(arglist,"is not implemented")
-            print_usage()
+    
+    def sort(self, arg):
+        ''' return the transactions sorted by the given argument '''
+        return self.runQuery(f"SELECT rowid,* from dictName ORDER BY {arg} DESC",())   #change dictName when implemented
