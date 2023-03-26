9947c0ed4cb013d4c001094942387b486c34e98b
mnamadina <madina@brandeis.edu>
2023-03-26 15:53:15 -0400

    added some changes to my own methods

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -2,6 +2,7 @@
 import sys
 from transactions import Transactions
 
+tracker = Transactions('tracker.db')
 
 def process(arglist):
     ''' examine args and make appropriate calls to the database'''
@@ -70,24 +71,6 @@ def read_eval():
 read_eval()
 
 # created by Madina
-tracker = Transactions('tracker.db')
-
-def print_transactions(self):
-    ''' printing the transactions '''
-    self.c.execute("SELECT * FROM transactions")
-    transactions = self.c.fetchall()
-    for transaction in transactions:
-        print(transaction)
-
-def add(self, item, amount, category, date, description):
-    '''adding a new transaction with all the required fields'''
-    self.c.execute("INSERT INTO transactions (item, amount, category, date, description) VALUES (?, ?, ?, ?, ?)",
-        (item, amount, category, date, description))
-    self.conn.commit()
-    print("Transaction added successfully!")
-
-def delete_transaction(self, item):
-        '''deleting an existing transactions'''
-        self.c.execute("DELETE FROM transactions WHERE item=?", (item,))
-        self.conn.commit()
-        print("Transaction deleted successfully!")
+tracker.show_transactions()
+tracker.add_transaction('001', 100.0, 'groceries', '2022-03-26', 'Bought groceries at the supermarket')
+tracker.delete_transaction('001')
