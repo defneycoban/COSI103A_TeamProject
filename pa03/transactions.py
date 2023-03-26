009c948e22f98d915331b30d0940d7eb91f1649b
mnamadina <madina@brandeis.edu>
2023-03-26 15:48:14 -0400

    modified

--- pa03/transactions.py
+++ pa03/transactions.py
@@ -13,7 +13,26 @@ class Transactions():
         ''' return the transactions sorted by the given argument '''
         return self.runQuery(f"SELECT rowid,* from dictName ORDER BY {arg} DESC",())   #change dictName when implemented
     
-   
+    def show_transactions(self):
+        ''' printing the transactions '''
+        self.c.execute("SELECT * FROM transactions")
+        transactions = self.c.fetchall()
+        for transaction in transactions:
+            print(transaction)
+
+    def add_transaction(self, item, amount, category, date, description):
+        '''adding a new transaction with all the required fields'''
+        self.c.execute("INSERT INTO transactions (item, amount, category, date, description) VALUES (?, ?, ?, ?, ?)",
+            (item, amount, category, date, description))
+        self.conn.commit()
+        print("Transaction added successfully!")
+
+    def delete_transaction(self, item):
+        '''deleting an existing transactions'''
+        self.c.execute("DELETE FROM transactions WHERE item=?", (item,))
+        self.conn.commit()
+        print("Transaction deleted successfully!")
+
 
    
 
