011285d6a6dcdbce841aa10d5ab8ddd321136d12
mnamadina <madina@brandeis.edu>
2023-03-26 19:20:44 -0400

    added some pytest

--- pa03/test_transactions.py
+++ pa03/test_transactions.py
@@ -1,36 +1,53 @@
 # pytest tests for transactions.py
 from transactions import Transactions
 
-transaction = Transactions()
+t = Transactions('test.db')
 
-def test_add_transaction():
-    tracker = transaction('test.db')
-    tracker.add_transaction(1, 10, 'food', '2023-03-26', 'Lunch')
-    tracker.c.execute("SELECT * FROM transactions")
-    transaction = tracker.c.fetchone()
-    assert transaction == (1, 10, 'food', '2023-03-26', 'Lunch')
+def test_show_transactions(self):
+    # Add some transactions to the database
+    t.add({'title': 'Transaction 1', 'desc': 'Description 1', 'completed': False})
+    t.add({'title': 'Transaction 2', 'desc': 'Description 2', 'completed': True})
+    t.add({'title': 'Transaction 3', 'desc': 'Description 3', 'completed': False})
+    transactions = t.show_transactions()
 
-def test_show_transactions():
-    # Initialize Transaction object and add a transaction
-    tracker = transaction('test.db')
-    tracker.add_transaction(1, 10, 'food', '2023-03-26', 'Lunch')
+    assert len(transactions) == 3
+    assert transactions[0]['title'] == 'Transaction 1'
+    assert transactions[0]['desc'] == 'Description 1'
+    assert transactions[0]['completed'] == False
+    assert transactions[1]['title'] == 'Transaction 2'
+    assert transactions[1]['desc'] == 'Description 2'
+    assert transactions[1]['completed'] == True
+    assert transactions[2]['title'] == 'Transaction 3'
+    assert transactions[2]['desc'] == 'Description 3'
+    assert transactions[2]['completed'] == False
 
-    # Get the transactions as a list of strings
-    transactions = tracker.get_transactions_as_strings()
-
-    # Check that the transactions are formatted correctly
-    expected_output = 'Item: 1, Amount: 10, Category: food, Date: 2023-03-26, Description: Lunch\n'
-    assert transactions == expected_output
-
-def test_delete_transaction():
-    # Initialize Transaction object and add a transaction
-    tracker = transaction('test.db')
-    tracker.add_transaction(1, 10, 'food', '2023-03-26', 'Lunch')
-
-    # Delete the transaction
-    tracker.delete_transaction(1)
-
-    # Check that the transaction was deleted
-    tracker.c.execute("SELECT * FROM transactions")
-    transaction = tracker.c.fetchone()
-    assert transaction is None
+    t.delete(1)
+    t.delete(2)
+    t.delete(3)
+    
+def test_add(self):
+    # Add a transaction to the database
+    t.add({'title': 'Transaction 1', 'desc': 'Description 1', 'completed': False})
+    # Get the list of transactions
+    transactions = t.show_transactions() 
+    # Verify that the transaction was added correctly
+    assert len(transactions) == 1
+    assert transactions[0]['title'] == 'Transaction 1'
+    assert transactions[0]['desc'] == 'Description 1'
+    assert transactions[0]['completed'] == False
+    
+    # Delete the transaction from the database
+    t.delete(1)
+    
+def test_delete(self):
+    # Add a transaction to the database
+    t.add({'title': 'Transaction 1', 'desc': 'Description 1', 'completed': False})
+    
+    # Delete the transaction from the database
+    t.delete(1)
+    
+    # Get the list of transactions
+    transactions = t.show_transactions()
+    
+    # Verify that the transaction was deleted correctly
+    assert len(transactions) == 0
