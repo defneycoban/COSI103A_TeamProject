7fa7961cae602803c844a177ddf8d6b576ba41d3
Eliora Kruman <krumaneliora@gmail.com>
2023-03-26 21:58:09 -0400

    modified sort

--- pa03/test_transactions.py
+++ pa03/test_transactions.py
@@ -4,6 +4,30 @@ from transactions import Transactions
 def db():
     return Transactions('test.db')
 
+# def test_sort(db):
+#     # add some transactions
+#     db.add({'item #': 1, 'amount': 10.0, 'category': 'Food', 'date': '2022-02-26', 'description': 'Lunch'})
+#     db.add({'item #': 2, 'amount': 5.0, 'category': 'Transport', 'date': '2022-03-24', 'description': 'Bus fare'})
+#     db.add({'item #': 3, 'amount': 20.0, 'category': 'Shopping', 'date': '2023-01-27', 'description': 'Groceries'})
+#     # check if they are sorted by date
+#     rows = db.sort('date')
+#     assert len(rows) == 3
+#     assert rows[0]['item #'] == 3
+#     assert rows[1]['item #'] == 2
+#     assert rows[2]['item #'] == 1
+#     # check if they are sorted by month
+#     rows = db.sort('month')
+#     assert len(rows) == 3
+#     assert rows[0]['item #'] == 2
+#     assert rows[1]['item #'] == 1
+#     assert rows[2]['item #'] == 3
+#     #check if they are sorted by year
+#     rows = db.sort('year')
+#     assert len(rows) == 3
+#     assert rows[0]['item #'] == 3
+#     assert rows[1]['item #'] == 1
+#     assert rows[2]['item #'] == 2
+
 def test_add(db):
     # add a transaction
     db.add({'item #': 1, 'amount': 10.0, 'category': 'Food', 'date': '2022-03-26', 'description': 'Lunch'})
