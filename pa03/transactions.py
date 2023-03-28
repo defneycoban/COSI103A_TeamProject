9ffd4efaf3fec44142c51f94383278ce41fa4446
Eliora Kruman <krumaneliora@gmail.com>
2023-03-28 00:31:54 -0400

    fixed overwritten changes

--- pa03/transactions.py
+++ pa03/transactions.py
@@ -1,15 +1,18 @@
+'''this class is responsible for the transaction database and its operations'''
 import sqlite3
-import os
+
 
 #created by Defne
-def toDict(t):
+def to_dict(trans):
     ''' t is a tuple that gets converted to a dictionary'''
     transaction = {
-    'amount': t[1],
-    'date': t[2],
-    'description': t[3]
+    'id': trans[0],
+    'amount': trans[1],
+    'date': trans[2],
+    'description': trans[3]
     }
     return transaction
+
 
 # All sql commands should be done in this class. Print statements go in tracker.py
 class Transactions():
@@ -18,53 +21,50 @@ class Transactions():
     def __init__(self, path):
         ''' initialize the database '''
         self.path = path
-        self.runQuery('''CREATE TABLE IF NOT EXISTS transactions 
-                      (amount int, date text, description text)''', ())
-    
+        self.run_query('''CREATE TABLE IF NOT EXISTS transactions
+                     (amount int, date text, description text)''', ())
+
+
     # created by Eliora
     def sort(self, arg):
         ''' return the transactions sorted by the given argument '''
-        #version where user chooses month and that month is sorted
-        if(arg=='month'):
-            return self.runQuery(f"SELECT rowid,* from transactions where substr(date,6,2)={arg} DESC",())
-        if(arg=='year'):
-            return self.runQuery(f"SELECT rowid,* from transactions where substr(date,1,4)={arg} DESC",())
-        else:
-            return self.runQuery(f"SELECT rowid,* from transactions DESC",())
-        #version where all are shown sorted by month
-        # if(arg=='month'):
-        #     arg = arg[5:7]
-        # if(arg=='year'):
-        #     arg = arg[0:4]
-        # return self.runQuery(f"SELECT rowid,* from dictName ORDER BY {arg} DESC",())   #change dictName when implemented
-    
+        if len(arg)==2:
+            return self.run_query("""SELECT rowid,* from transactions where substr(date,6,2)=(?)
+                                 ORDER BY date DESC""",(arg,))
+        if len(arg)==4:
+            return self.run_query("""SELECT rowid,* from transactions where substr(date,1,4)=(?)
+                                 ORDER BY date DESC""",(arg,))
+        return self.run_query("SELECT rowid,* from transactions ORDER BY date DESC",())
+
+
     # created by Madina
     def show_transactions(self):
         ''' return all of the transactions as a list of dicts.'''
-        return self.runQuery("SELECT rowid,* from transactions",())
-    
+        return self.run_query("SELECT rowid,* from transactions",())
+
+
     # created by Madina
     def add(self,item):
         ''' create a transaction with all the fields needed and add it to the transactions table '''
-        return self.runQuery("INSERT INTO transactions VALUES(?,?,?)",(item['amount'],item['date'],item['description']))
+        return self.run_query("INSERT INTO transactions VALUES(?,?,?)"
+                             ,(item['amount'],item['date'],item['description']))
+
 
     # created by Madina
     def delete(self,rowid):
         ''' delete a transaction '''
-        return self.runQuery("DELETE FROM transactions WHERE rowid=(?)",(rowid,))
+        return self.run_query("DELETE FROM transactions WHERE rowid=(?)",(rowid,))
 
-   # created by Defne
-    def runQuery(self, query, tuple):
+
+    # created by Defne
+    def run_query(self, query, args):
         '''execute a SQLite command and return the result as a list of dictionaries'''
         # con= sqlite3.connect(os.getenv('HOME')+'/tracker.db')
         con = sqlite3.connect(self.path)
-        cur = con.cursor() 
-        cur.execute(query,tuple)
+        cur = con.cursor()
+        cur.execute(query, args)
         rows = cur.fetchall()
         con.commit()
         con.close()
-        return [toDict(row) for row in rows]
-    
-   
-
-    
+        return [to_dict(row) for row in rows]
+  
