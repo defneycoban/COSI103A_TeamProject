782a934961c6f3f9b02b2d6134cb221a57acca08
zev18 <zevross@gmail.com>
2023-05-07 11:56:21 -0400

    Added routes for zev

--- ca02/app/routes/prompt.js
+++ ca02/app/routes/prompt.js
@@ -1,12 +1,12 @@
 /*
   prompt.js -- Router for the Prompt
 */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Prompt = require('../models/Prompt')
-const User = require('../models/User')
-const ChatGPT = require('./chatgpt.js')
-
+const Prompt = require("../models/Prompt");
+const User = require("../models/User");
+const ChatGPT = require("./chatgpt.js");
+const { getResponse } = require("./gptPromptZev");
 
 /*
 this is a very simple server which maintains a key/value
@@ -14,95 +14,114 @@ store using an object where the keys and values are lists of strings
 
 */
 
-isLoggedIn = (req,res,next) => {
+isLoggedIn = (req, res, next) => {
   if (res.locals.loggedIn) {
-    next()
+    next();
   } else {
-    res.redirect('/login')
+    res.redirect("/login");
   }
-}
+};
 
 /* add the value in the body to the db associated to the key */
-router.post('/prompt',
-  isLoggedIn,
-  async (req, res, next) => {
-      const prompt = new Prompt(
-        {item:req.body.item,
-         createdAt: new Date(),
-         status: true,
-         userId: req.user._id
-        })
-      await todo.save();
-      res.redirect('/prompt')
+router.post("/prompt", isLoggedIn, async (req, res, next) => {
+  const prompt = new Prompt({
+    item: req.body.item,
+    createdAt: new Date(),
+    status: true,
+    userId: req.user._id,
+  });
+  await todo.save();
+  res.redirect("/prompt");
 });
 
-router.get('/villain', (req,res,next) => {
-  res.render('elioraPrompt')
-})
+router.get("/villain", (req, res, next) => {
+  res.render("elioraPrompt");
+});
 
-router.post('/villain',
-  async (req,res,next) => {
-    console.log('getting villain')
-    res.locals.prompt = req.body.prompt
-    result = await get_villain(req.body.prompt)
-    console.log('Result:', result); //result is currently undefined :(
-    res.render('response', {result})
-}
-)
+router.post("/villain", async (req, res, next) => {
+  console.log("getting villain");
+  res.locals.prompt = req.body.prompt;
+  result = await get_villain(req.body.prompt);
+  console.log("Result:", result); //result is currently undefined :(
+  res.render("response", { result });
+});
 
 //TODO: must be implemented
 const get_villain = async (prompt) => {
-  const message = 'What would be a good Dungeons and Dragons monster to fight against for:' + prompt;
+  const message =
+    "What would be a good Dungeons and Dragons monster to fight against for:" +
+    prompt;
   const response = await ChatGPT.getChatGPTResponse(message);
-  console.log('Response:', response); //response is currently undefined :(
+  console.log("Response:", response); //response is currently undefined :(
   return response;
 };
 
-router.get('/quest', (req,res,next) => {
-  res.render('madinaPrompt')
-})
+//////////////////////////////////////////////////
+/////////////// Zev's Code ///////////////////////
+//////////////////////////////////////////////////
 
-router.post('/quest',
-  async (req,res,next) => {
-    console.log('generating quest')
-    res.locals.prompt = req.body.prompt
-    result = await get_quest(req.body.prompt)
-    console.log('Result:', result); //result is currently undefined :(
-    res.render('response', {result})
-}
-)
+router.get("/setting", async (req, res, next) => {
+  res.render("zevPrompt");
+});
+
+router.post("/setting", async (req, res, next) => {
+  console.log("getting setting");
+  res.locals.prompt = req.body.prompt;
+  result = await getSetting(req.body.prompt);
+  console.log("Result:", result);
+  res.render("response", { result });
+});
+
+const getSetting = async (prompt) => {
+  const message =
+    "Give a dnd setting for the following character(s): " + prompt;
+  const response = await getResponse(message);
+  console.log("Response:", response);
+  return response;
+};
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+router.get("/quest", (req, res, next) => {
+  res.render("madinaPrompt");
+});
+
+router.post("/quest", async (req, res, next) => {
+  console.log("generating quest");
+  res.locals.prompt = req.body.prompt;
+  result = await get_quest(req.body.prompt);
+  console.log("Result:", result); //result is currently undefined :(
+  res.render("response", { result });
+});
 
 //TODO: must be implemented
 const get_quest = async (prompt) => {
-  const message = 'What could be a DnD quest for:' + prompt;
+  const message = "What could be a DnD quest for:" + prompt;
   const response = await ChatGPT.getChatGPTResponse(message);
-  console.log('Response:', response); //response is currently undefined :(
+  console.log("Response:", response); //response is currently undefined :(
   return response;
 };
 
+router.get("/prompt/byUser", isLoggedIn, async (req, res, next) => {
+  let results = await Prompt.aggregate([
+    {
+      $group: {
+        _id: "$userId",
+        total: { $count: {} },
+      },
+    },
+    { $sort: { total: -1 } },
+  ]);
 
-router.get('/prompt/byUser',
-  isLoggedIn,
-  async (req, res, next) => {
-      let results =
-            await Prompt.aggregate(
-                [ 
-                  {$group:{
-                    _id:'$userId',
-                    total:{$count:{}}
-                    }},
-                  {$sort:{total:-1}},              
-                ])
-              
-        results = 
-           await User.populate(results,
-                   {path:'_id',
-                   select:['username','age']})
+  results = await User.populate(results, {
+    path: "_id",
+    select: ["username", "age"],
+  });
 
-        //res.json(results)
-        res.render('summarizeByUser',{results})
+  //res.json(results)
+  res.render("summarizeByUser", { results });
 });
-
-
 
 module.exports = router;
